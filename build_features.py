ed2ccb1e70e83b440eea17ca1bed7f588abcbfdf
DaanHolleman <dholleman123@gmail.com>
2022-10-27 21:14:51 +0200

    Generate binary files

--- build_features.py
+++ build_features.py
@@ -43,7 +43,7 @@ def encode_word(word : str, freq : list, size : int = 16):
     if size is None: size = len(word)
     # match desired size
     word = word[:size]
-    word += ' ' * (size - len(word))
+    word += ' ' * int(size - len(word))
     # create integers corresponding to chars
     word_ints = [ freq.index(char) for char in word ]
     # create bytearray
@@ -53,22 +53,54 @@ def encode_word(word : str, freq : list, size : int = 16):
 def build_features(words : list, size : int = 16):
     N = len(words)
     freq = build_frequency(words)
-    features = np.zeros((N,size))
+    features = np.zeros((N,size), dtype=int)
     for index, word in enumerate(words):
         word_ints = np.array([int(b) for b in encode_word(word, freq, size) ])
         features[index] = word_ints
-    return features
+    return features, freq
 
-def save_features(fname : str, features : list):
-    freq = build_frequency(words)
-    for word in words:
-        b_array = encode_word(word, freq)
+def save_features(fname : str, features : list, freq : list):
+    writer = open(fname, 'wb')
+    # write freq key size to file
+    freq_size = len(freq).to_bytes(2, 'big')
+    writer.write(freq_size)
+    # write freq entries to file
+    for char in freq:
+        writer.write(bytes(char, encoding='ascii'))
+    # write features
+    
+    features_size = features.shape[0]
+    
+    writer.write(int(features_size).to_bytes(64, 'big'))
+    
+    for feature in features:
+        for integer in feature:
+            writer.write(int(integer).to_bytes(2, 'big'))
+    writer.close()
 
-def load_features(fname : str):
-    freq = ...
-    features = ...
-    return words, freq
 
+def load_features(fname : str, size : int = 16):
+    reader = open(fname, 'rb')
+    
+    # populate freq
+    freq_size = int.from_bytes(reader.read(2), 'big')
+    freq = [None]*freq_size
+    for i in range(freq_size):
+        freq[i] = reader.read(1).decode('ascii')
+    
+    # populate features
+    features_size = int.from_bytes(reader.read(64), 'big')
+    print(features_size)
+    features = np.zeros((features_size, size), dtype=int)
+    for i in range(features_size):
+        feature = [None]*size
+        for j in range(size):
+            feature[j] = int.from_bytes(reader.read(2), 'big')
+        features[i] = feature
+    return features, freq
 
+fname = 'elvish_features.bin'
 words = cel.get_words()
-features = build_features(words, 16)
+features, freq = build_features(words, 16)
+save_features(fname, features, freq)
+l_feat, l_freq = load_features(fname, 16)
